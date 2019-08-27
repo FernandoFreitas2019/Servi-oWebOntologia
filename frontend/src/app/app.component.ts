863456194520ba83de53b2fbc0b12e407db84747
Fernando <fernandoeedimar@yahoo.com.br>
2019-08-27 19:19:55 -0300

    Front end

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, RequestOptionsArgs, Headers, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  private sparkqlData = null;
+  uri: any;
+  er:any;
+  
+  constructor(private http: Http) {
+
+
+  }
+
+  sparkql(query) {
+    if (!query || query == "") {
+      alert("Query vazia!");
+      return;
+
+    }
+    let headers: Headers = new Headers({
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    });
+
+
+    let options: RequestOptionsArgs = {
+      headers: headers,
+    };
+
+    this.http.get('http://localhost:3030/ds/query?query=' + query, options) // 1
+      .map(response => response.json())
+      .subscribe(data => {
+        console.log(data);
+
+
+        for (var i = 0; i < data.results.bindings.length; i++) {
+          data.results.bindings[i].type.value = data.results.bindings[i].type.value.replace(this.uri, "");
+        }
+        this.sparkqlData = data.results.bindings; // 3
+      },
+      error => {
+       this.er=error._body; 
+        
+      });
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
